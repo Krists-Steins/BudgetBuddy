bf0a2719907147800c5feef6b5ea972014127e1c
Krists Šteins <steins.krists@jak.lv>
2026-05-29 09:05:14 +0300

    Made the Main class, so the user doesn't have to find the main window (LoginRegistrationWindow).

---
+++ BudgetBuddy/src/me/techii/Main.java
@@ -0,0 +1,11 @@
+package me.techii;
+
+import me.techii.frames.LoginRegistrationWindow;
+
+public class Main {
+	public static void main(String[] args) {
+		LoginRegistrationWindow window = new LoginRegistrationWindow();
+		window.setTitle("Login/Registration Form - BudgetBuddy");
+		window.setVisible(true);
+	}
+}
